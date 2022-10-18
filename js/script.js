7bbb9bfde5fc5a792418b6a7cd90da07b8814358
DmitryTruten <dmitriy2503a@gmail.com>
2022-10-18 16:30:40 +0300

    add two functions for displaying values

--- js/script.js
+++ js/script.js
@@ -1,40 +1,55 @@
+//  Model
 const calculator = {
-  displayValue: '0',
+  displayValue: "0",
   firstOperand: null,
   waitingForSecondOperand: false,
   operator: null,
 };
 
+// Controller
+const buttons = document.querySelector(".buttons-container");
+buttons.addEventListener("click", (event) => {
+  const { target } = event;
+
+  if (!target.matches("button")) {
+    return;
+  }
+
+  if (target.classList.contains("operator")) {
+    console.log("operator", target.value);
+    return;
+  }
+
+  if (target.classList.contains("decimal")) {
+    inputDecimal(target.value);
+    renderDisplay();
+    return;
+  }
+
+  if (target.classList.contains("all-clear")) {
+    console.log("clear", target.value);
+    return;
+  }
+  inputDigit(target.value);
+  renderDisplay();
+});
+
+function inputDigit(digit) {
+  const { displayValue } = calculator;
+  // Overwrite displayValue if the current value is 0
+  calculator.displayValue = displayValue === "0" ? digit : displayValue + digit;
+}
+
+function inputDecimal(dot) {
+  if (!calculator.displayValue.includes(dot)) {
+    calculator.displayValue += dot;
+  }
+}
+
+// View
 function renderDisplay() {
-  const display = document.querySelector('.calculator-screen');
+  const display = document.querySelector(".calculator-screen");
   display.value = calculator.displayValue;
 }
 
 renderDisplay();
-
-const buttons = document.querySelector('.buttons-container');
-buttons.addEventListener('click', (event) => {
-  
-  const { target } = event;
-
-  if (!target.matches('button')) {
-    return;
-  }
-
-  if (target.classList.contains('operator')) {
-    console.log('operator', target.value);
-    return;
-  }
-
-  if (target.classList.contains('decimal')) {
-    console.log('decimal', target.value);
-    return;
-  }
-
-  if (target.classList.contains('all-clear')) {
-    console.log('clear', target.value);
-    return;
-  }
-
-  console.log('digit', target.value);
-});
