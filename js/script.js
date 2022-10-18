3b8dad72c8785d0b5ff78a4fdbbd6d93ba2df22e
DmitryTruten <dmitriy2503a@gmail.com>
2022-10-18 11:35:49 +0300

    properly working printNumber function

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const buttonsContainer = document.querySelectorAll(".buttons-container");
 const calculatorScreen = document.getElementById('screen')
 
 const printNumber = event => {
+  calculatorScreen.value += event.target.value;
   console.log(event.target.value)
 }
 
