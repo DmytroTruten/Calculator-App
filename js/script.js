378889b169639203bcebae5a565f9a37ecb5b266
DmitryTruten <dmitriy2503a@gmail.com>
2022-10-19 17:09:28 +0300

    add calculate function

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ buttons.addEventListener("click", (event) => {
   }
 
   if (target.classList.contains("operator")) {
-    handleOperator(target.value)
+    handleOperator(target.value);
     renderDisplay();
     return;
   }
@@ -36,16 +36,30 @@ buttons.addEventListener("click", (event) => {
   renderDisplay();
 });
 
+function calculate(firstOperand, secondOperand, operator) {
+  if (operator === "+") {
+    return firstOperand + secondOperand;
+  } else if (operator === "-") {
+    return firstOperand - secondOperand;
+  } else if (operator === "*") {
+    return firstOperand * secondOperand;
+  } else if (operator === "/") {
+    return firstOperand / secondOperand;
+  }
+  return secondOperand;
+}
+
 function inputDigit(digit) {
   const { displayValue, waitingForSecondOperand } = calculator;
-  if(waitingForSecondOperand === true) {
+  if (waitingForSecondOperand === true) {
     calculator.displayValue = digit;
     calculator.waitingForSecondOperand = false;
   } else {
     // Overwrite displayValue if the current value is 0
-    calculator.displayValue = displayValue === "0" ? digit : displayValue + digit;
+    calculator.displayValue =
+      displayValue === "0" ? digit : displayValue + digit;
   }
-  console.log(calculator)
+  console.log(calculator);
 }
 
 function inputDecimal(dot) {
@@ -55,17 +69,22 @@ function inputDecimal(dot) {
 }
 
 function handleOperator(nextOperator) {
-  const { displayValue, firstOperand } = calculator;
+  const { displayValue, firstOperand, operator } = calculator;
   // parseFloat converts the string contents of 'displayValue' to floating number
   const inputValue = parseFloat(displayValue);
   // verify that 'firstOperand' is null and 'inputValue' isn't a NaN value
   if (firstOperand === null && !isNaN(inputValue)) {
     // update the firstOperand property
     calculator.firstOperand = inputValue;
+  } else if(operator) {
+    const result = calculate(firstOperand, inputValue, operator);
+
+    calculator.displayValue = String(result);
+    calculator.firstOperand = result;
   }
   calculator.waitingForSecondOperand = true;
   calculator.operator = nextOperator;
-  console.log(calculator)
+  console.log(calculator);
 }
 
 function clearInput() {
@@ -73,7 +92,7 @@ function clearInput() {
   calculator.firstOperand = null;
   calculator.waitingForSecondOperand = false;
   calculator.operator = null;
-  console.log(calculator)
+  console.log(calculator);
 }
 
 // View
