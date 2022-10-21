5bf3aba1fb2408f3626739b81c41207f39dce0ec
DmitryTruten <dmitriy2503a@gmail.com>
2022-10-21 12:46:28 +0300

    fix sqrt result type

--- js/script.js
+++ js/script.js
@@ -120,7 +120,7 @@ function getSquareRoot() {
       }
     });
   } else {
-    calculator.displayValue = result;
+    calculator.displayValue = result.toString();
   }
   console.log(calculator);
 }
